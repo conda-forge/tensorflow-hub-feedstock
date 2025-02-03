c9700afc108094e1562803e2c52000d01ad9997e
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2025-02-04 03:43:42 +0900

    In progress for build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 set -exuo pipefail
 
-WHEEL_DIR="./wheel_dir"
+WHEEL_DIR=${PWD}/wheel_dir
 mkdir -p ${WHEEL_DIR}
 bazel build tensorflow_hub/pip_package:build_pip_package
 bazel-bin/tensorflow_hub/pip_package/build_pip_package ${WHEEL_DIR}
