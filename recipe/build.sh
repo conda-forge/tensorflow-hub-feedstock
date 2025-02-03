c88337528655f616a6696bdf85a2a145d58e0c8c
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2025-02-04 03:28:06 +0900

    Try fixing script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-WHEEL_DIR=${PWD}/wheel_dir
+set -exuo pipefail
+
+WHEEL_DIR="./wheel_dir"
 mkdir -p ${WHEEL_DIR}
 bazel build tensorflow_hub/pip_package:build_pip_package
 bazel-bin/tensorflow_hub/pip_package/build_pip_package ${WHEEL_DIR}
