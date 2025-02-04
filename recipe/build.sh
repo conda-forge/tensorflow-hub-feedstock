959f2620797595af0bfc447974a0b59c64f84c52
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2025-02-04 14:48:32 +0900

    Try build fix

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,6 @@ set -exuo pipefail
 
 WHEEL_DIR=${PWD}/wheel_dir
 mkdir -p ${WHEEL_DIR}
-bazel build tensorflow_hub/pip_package:build_pip_package
+bazel build --enable_runfiles tensorflow_hub/pip_package:build_pip_package
 bazel-bin/tensorflow_hub/pip_package/build_pip_package ${WHEEL_DIR}
 ${PYTHON} -m pip install --no-deps ${WHEEL_DIR}/*.whl
