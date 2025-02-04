2fa1773948b418e0ae491d4930088489ace1f0e6
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2025-02-04 15:00:30 +0900

    Improve syntax

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,5 @@ set -exuo pipefail
 WHEEL_DIR=${PWD}/wheel_dir
 mkdir -p ${WHEEL_DIR}
 bazel build --enable_runfiles tensorflow_hub/pip_package:build_pip_package
-bazel-bin/tensorflow_hub/pip_package/build_pip_package ${WHEEL_DIR}
+bazel run tensorflow_hub/pip_package/build_pip_package ${WHEEL_DIR}
 ${PYTHON} -m pip install --no-deps ${WHEEL_DIR}/*.whl
