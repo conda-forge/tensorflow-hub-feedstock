ba19b05ac64458094294b0b6b664903719ff986b
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2025-02-04 15:38:09 +0900

    Fix scripts

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-set -exuo pipefail
+set -e
 
 WHEEL_DIR=${PWD}/wheel_dir
 mkdir -p ${WHEEL_DIR}
-bazel run tensorflow_hub/pip_package:build_pip_package ${WHEEL_DIR}
+bazel build tensorflow_hub/pip_package:build_pip_package
+bazel-bin/tensorflow_hub/pip_package/build_pip_package ${WHEEL_DIR}
 ${PYTHON} -m pip install --no-deps ${WHEEL_DIR}/*.whl
